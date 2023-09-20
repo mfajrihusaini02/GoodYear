a4f24d808cfc4e7a1ba6b3bc722f80741030d29b
Jamiko Arshal <arshaljamiko@gmail.com>
2023-09-20 14:56:26 +0700

    Fixing Edit Disable

--- app/Models/TransaksiModel.php
+++ app/Models/TransaksiModel.php
@@ -7,7 +7,7 @@ use CodeIgniter\Model;
 class TransaksiModel extends Model
 {
     protected $table = 'transaksi_sertifikat';
-    protected $primaryKey = 'id_transaksi';
+    protected $primaryKey = 'id_karyawan';
     protected $useAutoIncrement = true;
     protected $allowedFields = [
         'id_transaksi',
@@ -35,7 +35,7 @@ class TransaksiModel extends Model
         return $this->db->table('sertifikat')
             ->join('transaksi_sertifikat', 'transaksi_sertifikat.id_sertifikat=sertifikat.id_sertifikat')
             ->join('karyawan', 'karyawan.id_karyawan=transaksi_sertifikat.id_karyawan')
-            ->where('nik', $id_karyawan)
+            ->where('transaksi_sertifikat.id_karyawan', $id_karyawan)
             ->get()->getResultArray();
     }
 }
